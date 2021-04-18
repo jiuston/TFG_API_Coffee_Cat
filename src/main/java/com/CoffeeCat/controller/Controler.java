0c38a06df8dd2708974947522d5716f03dc5ff3b
Jiuston <daw011.2020@gmail.com>
2021-04-18 20:58:36 +0200

    Arreglado controler gato

--- src/main/java/com/CoffeeCat/controller/Controler.java
+++ src/main/java/com/CoffeeCat/controller/Controler.java
@@ -3,12 +3,15 @@ package com.CoffeeCat.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class Controler {
 
     @GetMapping("/")
-    public String hola(){
-        return "hola";
+    public void hola(HttpServletResponse response) throws IOException {
+        response.sendRedirect("swagger-ui.html#/");
     }
 
 }

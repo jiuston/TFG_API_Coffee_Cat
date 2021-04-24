47b945fd7f732cd1f6b94b5c28e1d8309f0b6ca3
Jiuston <daw011.2020@gmail.com>
2021-04-24 11:09:17 +0200

    Todo funciona bien

--- src/main/java/com/CoffeeCat/controller/GatoController.java
+++ src/main/java/com/CoffeeCat/controller/GatoController.java
@@ -2,24 +2,19 @@ package com.CoffeeCat.controller;
 
 import com.CoffeeCat.modelo.gato.Gato;
 import com.CoffeeCat.modelo.gato.GatoOutputDTO;
-import com.CoffeeCat.modelo.gato.Sexo;
 import com.CoffeeCat.service.GatoService;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE, RequestMethod.PUT})

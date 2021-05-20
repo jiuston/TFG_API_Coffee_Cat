83bdeebcebee43a0f8b74f8c8cf53419b0ae5428
Jiuston <daw011.2020@gmail.com>
2021-05-20 18:32:54 +0200

    Arreglado el PutGato

--- src/main/java/com/CoffeeCat/service/GatoService.java
+++ src/main/java/com/CoffeeCat/service/GatoService.java
@@ -4,30 +4,34 @@ import com.CoffeeCat.modelo.gato.Gato;
 import com.CoffeeCat.modelo.gato.Sexo;
 import com.CoffeeCat.repository.GatoRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Service
-public class GatoService extends BaseService<Gato, String, GatoRepository>{
+public class GatoService extends BaseService<Gato, String, GatoRepository> {
 
-    public Gato crearGato(Gato gato, String nombre, String fecha_nacimiento,  String sexo,  String historia, MultipartFile file) throws IOException {
-        gato.setNombre(nombre);
-        try {
-            Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fecha_nacimiento);
-            gato.setFecha_nacimiento(fecha);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+    public Gato crearGato(Gato gato, String nombre, String fecha_nacimiento, String sexo, String historia, MultipartFile file) throws IOException {
+        if (nombre != null)
+            gato.setNombre(nombre);
+        if (fecha_nacimiento != null)
+            try {
+                Date fecha = new SimpleDateFormat("dd/MM/yyyy").parse(fecha_nacimiento);
+                gato.setFecha_nacimiento(fecha);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        if (sexo!=null)
         gato.setSexo(Sexo.getSexo(sexo));
+        if (historia!=null)
         gato.setHistoria(historia);
-        byte[] bytesImagen= file.getBytes();
-        gato.setImagen(bytesImagen);
+        if (file!=null){
+            byte[] bytesImagen = file.getBytes();
+            gato.setImagen(bytesImagen);
+        }
         return gato;
     }
 }
